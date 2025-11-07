ba8576ca6e7152697baddffd52bf87db5677396a
abrahamsil626 <silhernandez.223@gmail.com>
2025-11-06 20:26:24 -0500

    Agrega paso de build al workflow de deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://abrahamsil626.github.io',
+  base: 'C9200L-24P-4G-E',
   vite: {
     plugins: [tailwindcss()]
   }
